0c88bae263a2606bdd4b6ca86cb870275c178718
Mohan Pakkurti <mohan.pakkurti@gmail.com>
2003-09-30 12:10:11 +0000

    Oracle9i porting changes   - Changed procedure names from delete to del   - Changed view declaration to use actual column names   - Changed "rel_constraints_violated_one" and "rel_constraints_violated_two"     (http://openacs.org/forums/message-view?message_id=81045)

--- sql/oracle/shipping-gateway-sc-drop.sql
+++ sql/oracle/shipping-gateway-sc-drop.sql
@@ -1,11 +1,11 @@
 begin
-    acs_sc_operation.delete('ShippingGateway','RatesAndServicesSelection');
-    acs_sc_msg_type.delete('ShippingGateway.RatesAndServicesSelection.InputType');
-    acs_sc_msg_type.delete('ShippingGateway.RatesAndServicesSelection.OutputType');
-    acs_sc_operation.delete('ShippingGateway','ServiceDescription');
-    acs_sc_msg_type.delete('ShippingGateway.ServiceDescription.InputType');
-    acs_sc_msg_type.delete('ShippingGateway.ServiceDescription.OutputType');
-    acs_sc_contract.delete('ShippingGateway');
+    acs_sc_operation.del('ShippingGateway','RatesAndServicesSelection');
+    acs_sc_msg_type.del('ShippingGateway.RatesAndServicesSelection.InputType');
+    acs_sc_msg_type.del('ShippingGateway.RatesAndServicesSelection.OutputType');
+    acs_sc_operation.del('ShippingGateway','ServiceDescription');
+    acs_sc_msg_type.del('ShippingGateway.ServiceDescription.InputType');
+    acs_sc_msg_type.del('ShippingGateway.ServiceDescription.OutputType');
+    acs_sc_contract.del('ShippingGateway');
 end;
 /
 show errors
